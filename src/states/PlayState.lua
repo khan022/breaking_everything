5a3b727352b41fddac52c6fbfbf8851f631bba63
Shafkat Khan Siam <sk.siam@outlook.com>
2023-09-18 13:01:32 +0600

    collision updating with speed control v0.30

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -38,7 +38,21 @@ function PlayState:update(dt)
 
     -- check collision
     if self.ball:collides(self.paddle) then
+        -- raise the ball above the paddle
+        self.ball.y = self.paddle.y - 8
         self.ball.dy = -self.ball.dy
+
+        -- tweak the angle of the ball based on the position it hits the paddle
+
+        -- if we hit the paddle on the left side
+        if self.ball.x < self.paddle.x + (self.paddle.width / 2) and self.paddle.dx < 0 then
+            self.ball.dx = -50 + -(8 * (self.paddle.x + self.paddle.width / 2 - self.ball.x))
+
+        -- if we hit the paddle on the right side
+        elseif self.ball.x > self.paddle.x + (self.paddle.width / 2) and self.paddle.dx > 0 then
+            self.ball.dx = 50 + (8 * math.abs(self.paddle.x + self.paddle.width / 2 - self.ball.x))
+        end
+
         gSounds['paddle-hit']:play()
     end
 
@@ -48,6 +62,37 @@ function PlayState:update(dt)
         if brick.inPlay and self.ball:collides(brick) then
             -- trigger the brick hit function
             brick:hit()
+
+            -- collision codes for bricks
+            -- left edge
+            if self.ball.x + 2 < brick.x and self.ball.dx > 0 then
+                -- flip the x velocity and reset the position of the ball
+                self.ball.dx = -self.ball.dx
+                self.ball.x = brick.x - 8
+            -- right edge
+            elseif self.ball.x + 6 < brick.x + brick.width and self.ball.dx < 0 then
+                -- flip the x velocity and reset the position of the ball
+                self.ball.dx = -self.ball.dx
+                self.ball.x = brick.x + 32
+            
+            -- top edge if no collision
+            elseif self.ball.y < brick.y then
+                -- flip y velocity and reset the position
+                self.ball.dy = -self.ball.dy
+                self.ball.y = brick.y - 8
+            
+            else
+
+                -- flip y velocity and reset position outside of brick
+                self.ball.dy = -self.ball.dy
+                self.ball.y = brick.y + 16
+            end
+
+            -- scale and increase the velocity of the ball
+            self.ball.dy = self.ball.dy * 1.02
+
+            -- only allowing collision with one brick, when the corner's hit
+            break
         end
     end
 
